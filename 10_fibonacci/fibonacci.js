cb04cca7bf6a9e7163f027177a74a692180a6b16
Lulu <112640408+HopeICanChangeMyUsername@users.noreply.github.com>
2023-08-22 16:32:31 -0300

    all done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(iterations) {
+//posso fazer com reduce ou simplesmente uma variavel pra dar tracking
+if (iterations < 0) return "OOPS"
+let nextNum
+const firstNums = [1,1]
+//repopular o array com a soma
+for (let i = 1; i<=iterations; i++)
+{
+  nextNum = firstNums[i-1] + firstNums[i]    
+  firstNums.push(nextNum)  
+}
+return firstNums[iterations-1]
+//sum with anterior
 
 };
 
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,30 @@
-const findTheOldest = function() {
-
-};
-
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear()
+    const oldestPersonInitial = {}
+  
+     return people.reduce((oldestPerson, currentPerson) => {
+     let subtractYear = ("yearOfDeath" in currentPerson)? currentPerson.yearOfDeath : currentYear
+     let currentAge = subtractYear - currentPerson.yearOfBirth
+     
+     if (!("name" in oldestPerson) || (currentAge > oldestPerson.age)) {
+      oldestPerson = {
+         name: currentPerson.name,
+      yearOfBirth: currentPerson.yearOfBirth,
+      yearOfDeath: currentPerson.yearOfDeath,
+      //I can use the spread operator here to copy the whole object and it will copy yearofdeath only if its present, not returning undefined
+      age: currentAge
+      //i can delete the age when returning the object.
+     }
+     console.log(oldestPerson)
+     return oldestPerson
+     }
+     else {
+     console.log(oldestPerson)
+     return oldestPerson 
+     }
+     },oldestPersonInitial)
+    
+  
+  }
 // Do not edit below this line
 module.exports = findTheOldest;
